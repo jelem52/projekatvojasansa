5fbd3b31fb054eb87c171b8ba8da1cf199cf46b8
jelem52 <tvojaasansa@gmail.com>
2025-07-13 17:11:52 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -179,32 +179,32 @@ function App() {
             {[
               {
                 icon: <MessageCircle className="w-8 h-8" />,
-                title: "Komunikacija",
+                title: "Veština Komunikacije",
                 description: "Naučićeš kako da pričaš sa devojkama bez da mucaš i zaboravljaš šta si hteo da kažeš."
               },
               {
                 icon: <Video className="w-8 h-8" />,
-                title: "Lekcije",
+                title: "Video Lekcije",
                 description: "Jasne lekcije, u kojima ćeš videti detaljna objašnjenja i realne primere."
               },
               {
                 icon: <Zap className="w-8 h-8" />,
-                title: "Samopouzdanje",
+                title: "Izgradnja Samopouzdanja",
                 description: "Pokazaćemo ti tehnike uz pomoć kojih se više nećeš znojiti kad treba da platiš konobaricu za račun."
               },
               {
                 icon: <Heart className="w-8 h-8" />,
-                title: "Privlačnosti",
+                title: "Psihologija Privlačnosti",
                 description: "Postaćeš genije za razumevanje ženskog uma, i saznaćeš šta ih zapravo privlači."
               },
               {
                 icon: <Video className="w-8 h-8" />,
-                title: "Lekcije",
+                title: "Video Lekcije",
                 description: "Jasne lekcije, u kojima ćeš videti detaljna objašnjenja i realne primere."
               },
               {
                 icon: <Target className="w-8 h-8" />,
-                title: "Rezultati",
+                title: "Konkretni Rezultati",
                 description: "Videćeš konkretne rezultate čim kreneš da primenjuješ znanje."
               }
             ].map((item, index) => (
